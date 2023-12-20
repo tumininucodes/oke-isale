334259f948afeca37baa60165f376516b6157ffd
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-12-20 06:50:46 +0100

    feat: read files upload

--- api/files/files.go
+++ api/files/files.go
@@ -1,7 +1,39 @@
 package files
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func FetchFiles(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "Get all users"})
+	
+	uploadsDir := "storage/"
+
+	files, err := os.ReadDir(uploadsDir)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read directory"})
+		return
+	}
+
+	var fileList []gin.H
+
+	for _, file := range files {
+		fileInfo, err := file.Info()
+		if err != nil {
+			fmt.Println("Error getting file info:", err)
+			continue
+		}
+
+		fileData := gin.H{
+			"name": file.Name(),
+			"size": fmt.Sprintf("%.2fKB", float64(fileInfo.Size())/1024), // Convert size to kilobytes
+		}
+
+		fileList = append(fileList, fileData)
+	}
+
+	c.JSON(http.StatusOK, fileList)
 }
